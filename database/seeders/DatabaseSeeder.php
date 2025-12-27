6104d3d606d7870dbcc20f4f602099be1827949d
Felipe <feanselmouk@gmail.com>
2025-12-27 10:13:00 +0000

    changes on factory and seeder for user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name'  => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class
         ]);
+    
     }
 }
+
+//roda sempre o DatabaseSeeder por padrao
