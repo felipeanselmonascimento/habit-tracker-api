8b337f7b1193af8c786d5ebbe192bfe50db471d3
Felipe <feanselmouk@gmail.com>
2025-12-27 09:53:58 +0000

    added 2 models and respectives migrations

---
+++ database/migrations/2025_12_27_094244_create_habit_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Habits;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('habit_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->foreignIdFor(Habits::class)->constrained();
+            $table->datetime('completed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('habit_logs');
+    }
+};
